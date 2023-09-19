ab1a08c3b49b9d15042d394ee6f96c8d7ce94241
Marina <mator987@list.ru>
2023-09-19 15:37:55 +0500

    Добавляет функцию активацию форм и инпутов

--- js/form.js
+++ js/form.js
@@ -1,6 +1,8 @@
 const adForm = document.querySelector('.ad-form');
 const mapForm = document.querySelector('.map__filters');
 
+// Функция, которая перводит поля ввода, кнопки и текстовые поля форм в НЕактивное состояние
+
 const setDisabledInput = (form) => {
   const inputs = form.querySelectorAll('input');
   const selects = form.querySelectorAll('select');
@@ -12,6 +14,21 @@ const setDisabledInput = (form) => {
   });
 };
 
+// Функция, которая перводит поля ввода, кнопки и текстовые поля форм в активное состояние
+
+const setEnabledInput = (form) => {
+  const inputs = form.querySelectorAll('input');
+  const selects = form.querySelectorAll('select');
+  const textareas = form.querySelectorAll('textarea');
+  const buttons = form.querySelectorAll('button');
+
+  [...inputs, ...selects, ...textareas, ...buttons].forEach((element) => {
+    element.disabled = false;
+  });
+};
+
+// Перевод формы в НЕактивную форму + дезактивация полей
+
 const disableForm = () => {
   adForm.classList.add('ad-form--disabled');
   mapForm.classList.add('map__filters--disabled');
@@ -20,6 +37,16 @@ const disableForm = () => {
   setDisabledInput(mapForm);
 };
 
-export { disableForm };
+// Перевод формы в активную форму + активация полей
+
+const enableForm = () => {
+  adForm.classList.remove('ad-form--disabled');
+  mapForm.classList.remove('map__filters--disabled');
+
+  setEnabledInput(adForm);
+  setEnabledInput(mapForm);
+};
+
+export { disableForm, enableForm };
 
 
